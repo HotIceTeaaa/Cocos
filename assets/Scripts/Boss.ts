b3920ceb7a221d8f43430e79f4cbc21d4d199f83
unknown <mikeaudric123@gmail.com>
2026-04-23 16:15:36 +0700

    boss ngikutin player, udah pake lag

--- assets/Scripts/Boss.ts
+++ assets/Scripts/Boss.ts
@@ -1,21 +1,25 @@
-import { _decorator, Component, Collider2D, Contact2DType, IPhysics2DContact, Sprite } from 'cc';
+import { _decorator, Component, Collider2D, Contact2DType, IPhysics2DContact, Sprite, Node, math } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Boss')
 export class Boss extends Component {
-
     @property
     hp: number = 3;
 
     @property
     private modeSeruduk: boolean = false;
 
+    @property({ type: Node })
+    playerNode: Node | null = null;
+
+    @property
+    followSpeed: number = 2.0;
+
     private spriteComponent: Sprite = null;
 
     onLoad() {
         this.spriteComponent = this.getComponent(Sprite);
         
-        //Collider
         const collider = this.getComponent(Collider2D);
         if (collider) {
             console.log("invader collider set")
@@ -32,10 +36,18 @@ export class Boss extends Component {
         this.hp -= 1;
         console.log("Bos kena hit! Sisa HP: " + this.hp);
 
-        // Cek kalau HP sudah habis
         if (this.hp <= 0) {
             console.log("Bos Hancur!");
             this.node.destroy();
+        }
+    }
+
+    update(deltaTime: number) {
+        if (this.playerNode) {
+            let targetX = this.playerNode.position.x;
+            let currentX = this.node.position.x;
+            let newX = math.lerp(currentX, targetX, deltaTime * this.followSpeed);
+            this.node.setPosition(newX, this.node.position.y, this.node.position.z);
         }
     }
 }
